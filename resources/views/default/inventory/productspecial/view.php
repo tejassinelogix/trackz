21a601ef7b22059f95e0bb4a33d09bd2030dce63
Tejas Soni <tejas.soni@sinelogix.com>
2020-04-24 18:55:03 +0530

    product discount & product special

--- resources/views/default/inventory/productspecial/view.php
+++ resources/views/default/inventory/productspecial/view.php
@@ -1,6 +1,6 @@
 <?php
-$title_meta = 'CustomerGroup Listing for Your Tracksz Store, a Multiple Marketplace Management Service';
-$description_meta = 'CustomerGroup Listing for your Tracksz Store, a Multiple Marketpalce Management Service';
+$title_meta = 'Product Special Listing for Your Tracksz Store, a Multiple Marketplace Management Service';
+$description_meta = 'Product Special Listing for your Tracksz Store, a Multiple Marketpalce Management Service';
 ?>
 <?= $this->layout('layouts/backend', ['title' => $title_meta, 'description' => $description_meta]) ?>
 
@@ -15,9 +15,9 @@ $description_meta = 'CustomerGroup Listing for your Tracksz Store, a Multiple Ma
             <header class="page-title-bar">
                 <!-- title -->
                 <div class="mb-3 d-flex justify-content-between">
-                    <h1 class="page-title"> <?= _('CustomerGroup Listing') ?> </h1>
+                    <h1 class="page-title"> <?= _('Product Special Listing') ?> </h1>
                 </div>
-                <p class="text-muted"> <?= _('This is where you can modify, and delete CustomerGroup for the current Active Store: ') ?><strong> <?= \Delight\Cookie\Cookie::get('tracksz_active_name') ?></strong></p>
+                <p class="text-muted"> <?= _('This is where you can modify, and delete Product Special for the current Active Store: ') ?><strong> <?= \Delight\Cookie\Cookie::get('tracksz_active_name') ?></strong></p>
                 <?php if (isset($alert) && $alert) : ?>
                     <div class="col-sm-12 alert alert-<?= $alert_type ?> text-center"><?= $alert ?></div>
                 <?php endif ?>
@@ -30,32 +30,36 @@ $description_meta = 'CustomerGroup Listing for your Tracksz Store, a Multiple Ma
                         <!-- .card-body starts -->
                         <?php
 
-                        if (is_array($all_customergroup) &&  count($all_customergroup) > 0) : ?>
-                            <table id="customergroup_table" name="customergroup_table" class="table table-striped table-bordered nowrap" style="width:100%">
+                        if (isset($all_productspecial) && is_array($all_productspecial) &&  count($all_productspecial) > 0) : ?>
+                            <table id="productspecial_table" name="productspecial_table" class="table table-striped table-bordered nowrap" style="width:100%">
                                 <thead>
                                     <tr>
-                                        <th><?= _('Name') ?></th>
-                                        <th><?= _('Description') ?></th>
-                                        <th><?= _('Approval') ?></th>
-                                        <th><?= _('SortOrder') ?></th>
+                                        <th><?= _('Product') ?></th>
+                                        <th><?= _('CustomerGroup') ?></th>
+                                        <th><?= _('Priority') ?></th>
+                                        <th><?= _('Price') ?></th>
+                                        <th><?= _('Date Start') ?></th>
+                                        <th><?= _('Date End') ?></th>
                                         <th><?= _('Action') ?></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach ($all_customergroup as $attributes) : ?>
+                                    <?php
+                                    foreach ($all_productspecial as $productspecial) : ?>
                                         <tr>
-                                            <td><?= $attributes['Name'] ?></td>
-                                            <td><?= $attributes['Description'] ?></td>
-                                            <td><?php echo ($attributes['Approval'] == 1) ? 'approved' : 'disapproved'; ?></td>
-                                            <td><?= $attributes['SortOrder'] ?></td>
+                                            <td><?= $productspecial['ProdName'] ?></td>
+                                            <td><?= $productspecial['CustGroupName'] ?></td>
+                                            <td><?= $productspecial['ProdPriority'] ?></td>
+                                            <td><?= $productspecial['ProdPrice'] ?></td>
+                                            <td><?= date("d-m-Y", strtotime($productspecial['ProdStartDate'])) ?></td>
+                                            <td><?= date("d-m-Y", strtotime($productspecial['ProdEndDate'])) ?></td>
                                             <td> <?php
                                                     $button = '';
-                                                    $edit_button = '<a href="' . \App\Library\Config::get('company_url') . '/customergroup/edit/' . $attributes['Id'] . '" class="btn btn-xs btn-warning btn_edit"><i class="far fa-edit"></i> Edit</a> &nbsp;';
-                                                    $delete_button = '<a href="#delete-' . $attributes['Id'] . '" delete_id="' . $attributes['Id'] . '" class="btn btn-xs btn-danger btn_delete"><i class="far fa-trash-alt"></i> Delete</a>';
+                                                    $edit_button = '<a href="' . \App\Library\Config::get('company_url') . '/productspecial/edit/' . $productspecial['ProdSpecId'] . '" class="btn btn-xs btn-warning btn_edit"><i class="far fa-edit"></i> Edit</a> &nbsp;';
+                                                    $delete_button = '<a href="#delete-' . $productspecial['ProdSpecId'] . '" delete_id="' . $productspecial['ProdSpecId'] . '" class="btn btn-xs btn-danger btn_delete"><i class="far fa-trash-alt"></i> Delete</a>';
                                                     $button .= $edit_button;
                                                     $button .= $delete_button;
                                                     echo $button;
-
                                                     ?></td>
                                         </tr>
                                     <?php endforeach; ?>
@@ -76,7 +80,7 @@ $description_meta = 'CustomerGroup Listing for your Tracksz Store, a Multiple Ma
 <script src="/assets/vendor/pace/pace.min.js"></script>
 <script src="/assets/vendor/stacked-menu/stacked-menu.min.js"></script>
 <script src="/assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script src="/assets/javascript/pages/customergroup.js"></script>
+<script src="/assets/javascript/pages/productspecial.js"></script>
 <?= $this->stop() ?>
 
 <?php $this->start('page_js') ?>
